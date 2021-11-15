8c77dec29ac1b6cf7f751ef76ad4537d6d76b334
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-11-15 15:31:32 +0000

    Day 8

---
+++ 50 Projects-in-50-days/Day 8/index.js
@@ -0,0 +1,12 @@
+let clock = document.getElementById("clock");
+clock.style.color = "#56D2F9"
+
+let date = new Date
+let hours = date.getHours()
+console.log(hours)
+let mins = date.getMinutes()
+let secs = date.getSeconds()
+let para = document.createElement("p")
+para.innerText = `${hours} : ${mins} : ${secs}`
+
+clock.appendChild(para);
