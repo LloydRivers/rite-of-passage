97d5d69a4e1d70651e16085c02064c5f90a5fdd0
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2021-12-20 10:35:23 +0000

    Logo animation complete.

--- 50 Projects-in-50-days/Day-13/script.js
+++ 50 Projects-in-50-days/Day-13/script.js
@@ -1,18 +1,29 @@
 let logoString = "hebe.";
-   let index = 0;
+   let index = 0, index2 = 4;
    let totalLength= 5;
+
 function printText(){
-    
    let logo = document.getElementById("logo");
    let count = logoString.length;
    if(index < count){
        logo.innerHTML += logoString.charAt(index);
        index++; 
+} else if(index2 >= 0){ 
+   let str = logo.innerText
+   str = str.substring(0, str.length - 1);
+   logo.innerText = str
+      index2--; 
+}
+else {
+   index = 0;
+   index2 = 4;
+   
+  
 }
 
 setTimeout(printText, 500)
-
 }
 printText();
+
 
 
