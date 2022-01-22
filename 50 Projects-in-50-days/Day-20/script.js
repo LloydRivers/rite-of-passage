48861e82cb9bdba9b85ccfa4cc9a305780b5ef74
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-01-22 12:30:33 +0000

    Middle column started

--- 50 Projects-in-50-days/Day-20/script.js
+++ 50 Projects-in-50-days/Day-20/script.js
@@ -5,5 +5,14 @@ getData(API_URL);
 async function getData(api) {
   const data = await fetch(api);
   const response = await data.json();
-  console.log(response.results[0]);
+  render(response);
+}
+
+function render(response) {
+  let heroCard = document.getElementById("hero-card");
+
+  let arr1 = response.results.slice(0, 1);
+  console.log(arr1);
+  let imagePath = arr1[0].poster_path;
+  heroCard.style.backgroundImage = `url(${IMG_PATH + imagePath})`;
 }
