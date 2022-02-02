057551ba30ef6fbe7410aa872e90920330ad76a0
LloydRivers <86473036+LloydRivers@users.noreply.github.com>
2022-02-02 20:50:46 +0000

    Hero started

--- 50 Projects-in-50-days/Day-22/day-22/src/components/Hero.js
+++ 50 Projects-in-50-days/Day-22/day-22/src/components/Hero.js
@@ -1,7 +1,27 @@
 import React from "react";
+import tooth from "../imgs/tooth.png";
+import needle from "../imgs/syringe.png";
 
 function Hero() {
-  return <div></div>;
+  return (
+    <main>
+      <section>
+        <div className="grid j-c-s-b col-2 global-width a-i-c">
+          <div className="hero-column">
+            <img className="icon-abs" src={needle} alt="" />
+            <span>FIND A DOCTOR EASY</span>
+            <h1>Let's Find Your Doctor</h1>
+            <p>
+              A Union of Compassion + Healthcare. Achieving better health care
+              one patient at a time.
+            </p>
+            <img className="tooth icon-abs" src={tooth} alt="" />
+          </div>
+          <div className="hero-column"></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Hero;
